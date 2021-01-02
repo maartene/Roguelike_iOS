c7884b503dcf4284adbc0311c945947591962cb7
Maarten Engels <maartene@mac.com>
2021-01-02 17:31:30 +0100

    add quick info view (debug help)

---
+++ Roguelike_iOS/UI/InfoView.swift
@@ -0,0 +1,21 @@
+//
+//  InfoView.swift
+//  Roguelike_iOS
+//
+//  Created by Maarten Engels on 02/01/2021.
+//  Copyright © 2021 thedreamweb. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
